a5b2f91fca45c08f2f3517b78ba1181b4427b854
Daniel Szoke <szokeasaurusrex@users.noreply.github.com>
2018-12-15 20:20:48 -0500

    Using setTimeout instead of setInterval

--- server_modules/periodicallyDeleteGames.js
+++ server_modules/periodicallyDeleteGames.js
@@ -4,47 +4,46 @@ const constants = require('../constants.js')
 const getDb = require('./db.js').getDb
 const getGamesCollection = require('./db.js').getGamesCollection
 
-function periodicallyDeleteGames (callback) {
+async function periodicallyDeleteGames (callback) {
   const db = getDb()
   const gamesCollection = getGamesCollection()
 
-  setInterval(async () => {
-    try {
-      const games = await gamesCollection.find({}).toArray()
-      const gameStatuses = await Promise.all(
-        Array.from(games, game => new Promise(async (resolve, reject) => {
-          try {
-            const statusCollection = (
-              await db.db('game-' + game.code).collection('status').findOne({})
-            )
-            resolve({
-              code: game.code,
-              status: statusCollection
-            })
-          } catch (e) {
-            reject(e)
-          }
-        }))
-      )
-      const dropCommands = []
-      const deletedGames = []
-      const currentDate = new Date()
-      gameStatuses.forEach(game => {
-        const shouldDie = (!game.status ||
-          currentDate - game.status.lastGameStart > constants.GAME_TTL)
-        if (shouldDie) {
-          deletedGames.push(game.code)
-          dropCommands.push(db.db('game-' + game.code).dropDatabase())
-          dropCommands.push(gamesCollection.deleteOne({ code: game.code }))
-          console.log('Deleted game ' + game.code)
+  try {
+    const games = await gamesCollection.find({}).toArray()
+    const gameStatuses = await Promise.all(
+      Array.from(games, game => new Promise(async (resolve, reject) => {
+        try {
+          const statusCollection = (
+            await db.db('game-' + game.code).collection('status').findOne({})
+          )
+          resolve({
+            code: game.code,
+            status: statusCollection
+          })
+        } catch (e) {
+          reject(e)
         }
-      })
-      await Promise.all(dropCommands)
-      callback(deletedGames)
-    } catch (e) {
-      console.error(e)
-    }
-  }, constants.GAME_TTL)
+      }))
+    )
+    const dropCommands = []
+    const deletedGames = []
+    const currentDate = new Date()
+    gameStatuses.forEach(game => {
+      const shouldDie = (!game.status ||
+        currentDate - game.status.lastGameStart > constants.GAME_TTL)
+      if (shouldDie) {
+        deletedGames.push(game.code)
+        dropCommands.push(db.db('game-' + game.code).dropDatabase())
+        dropCommands.push(gamesCollection.deleteOne({ code: game.code }))
+        console.log('Deleted game ' + game.code)
+      }
+    })
+    await Promise.all(dropCommands)
+    callback(deletedGames)
+  } catch (e) {
+    console.error(e)
+  }
+  setTimeout(() => periodicallyDeleteGames(callback), constants.GAME_TTL)
 }
 
 module.exports = periodicallyDeleteGames
